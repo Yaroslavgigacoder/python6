074bfa28f1a9e2d16708a931d8d71716f69ad697
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2023-02-03 22:26:44 +0300

    task101

--- №1.py
+++ №1.py
@@ -1,23 +1,27 @@
 def choose_coffee(*preference):
-    ingredients = [2, 3, 2]
+    ingredients = [4, 4, 0]
     print(ingredients)
     for i in range(len(preference)):
         if preference[i] == 'Эспрессо':
             if ingredients[0] >= 1:
-                return print('Эспрессо')
-        elif preference[i] == 'Капучино':
+                print('Эспрессо')
+        if preference[i] == 'Капучино':
             if ingredients[0] >= 1 and ingredients[1] >= 3:
-                return print('Капучино')
-        elif preference[i] == 'Маккиато':
+                print('Капучино')
+        if preference[i] == 'Маккиато':
             if ingredients[0] >= 2 and ingredients[1] >= 1:
-                return print('Маккиато')
-        elif preference[i] == 'Кофе-по-венски':
+                print('Маккиато')
+        if preference[i] == 'Кофе-по-венски':
             if ingredients[0] >= 1 and ingredients[2] >= 2:
-                return print('Кофе по-венски')
-        elif preference[i] == 'Латте Маккиато':
+                print('Кофе по-венски')
+        if preference[i] == 'Латте Маккиато':
             if ingredients[0] >= 1 and ingredients[1] >= 2 and ingredients[2] >= 1:
-                return print('Латте Маккиато')
-        elif preference[i] == 'Кон Панна':
+                print('Латте Маккиато')
+        if preference[i] == 'Кон Панна':
             if ingredients[0] >= 1 and ingredients[2] >= 1:
-                return print('Кон Панна')
+                print('Кон Панна')
     return ('К сожалению, не можем предложить Вам напиток')
+
+# coffe = input().split(', ')
+
+print(choose_coffee('Эспрессо','Маккиато', 'Капучино'))
